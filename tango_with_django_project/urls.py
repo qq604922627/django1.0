eb676aa24c3bd7228d391724a10c706224eba337
King <604922627@qq.com>
2019-02-01 15:34:43 +0800

    six

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -10,3 +10,11 @@ urlpatterns = [
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+#urlpatterns = [
+#    url(r'^$', views.index, name='index'),
+#    url(r'^about/$', views.about, name='about'),
+#    url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+#    views.show_category, name='show_category'),
+#]
